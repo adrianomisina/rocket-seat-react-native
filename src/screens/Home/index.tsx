55da16d29ae36a4c44981ade1c1480814df8ef63
Adriano Misina <adriano.misina@gmail.com>
2023-08-20 14:54:13 -0300

    input de dados

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,24 @@
+import { View, Text, TextInput } from 'react-native';
+import { styles } from './styles'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>  
+        <TextInput
+            style={styles.input}
+            placeholder='Nome do participante'
+            placeholderTextColor="#6b6b6b" 
+        />    
+    </View>
+  )
+}
+
+export default Home;
+
